9fedd0a1dda990bdd3991e0300a113d325c43149
giang-yb <ggvu2017@gmail.com>
2022-06-29 16:11:05 +0700

    update

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import TodoList from "./components/TodoList";
+import Textfield from "@atlaskit/textfield";
+import Button from "@atlaskit/button";
+import {useState, useCallback, useEffect} from 'react';
+import {v4} from 'uuid';
+
+const TODO_APP_STORAGE_KEY = 'TODO_APP';
+
+function App() {
+
+  const [todoList, setTodoList] = useState([]);
+  const [textInput, setTextInput] = useState('');
+
+  useEffect(()=> {
+    const storageTodoList = localStorage.getItem(TODO_APP_STORAGE_KEY);
+    if(storageTodoList){
+      setTodoList(JSON.parse(storageTodoList));
+    }
+  },[]);
+
+  useEffect(() => {
+    localStorage.setItem(TODO_APP_STORAGE_KEY, JSON.stringify(todoList));
+  }, [todoList]);
+  
+
+  const onTextInputChange = useCallback((e) => {
+    setTextInput(e.target.value);
+  }, []);
+
+  const onAddBtnClick = useCallback(
+    (e) => {
+      // them text input vao danh sach todoList
+      setTodoList([
+        { id: v4(), name: textInput, isCompleted: false },
+        ...todoList,
+      ]);
+
+      setTextInput("");
+    },
+    [textInput, todoList]
+  );
+
+  const onCheckBtnClick = useCallback((id) => {
+    setTodoList((prevState) =>
+      prevState.map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: true } : todo
+      )
+    );
+  }, []);
+
+  return (
+    <>
+      <h3>Things todo</h3>
+      <Textfield
+        name="add-todo"
+        placeholder="Add things todo..."
+        elemAfterInput={
+          <Button isDisabled={!textInput} appearance="primary" onClick={onAddBtnClick}>
+            Add
+          </Button>
+        }
+        css={{ padding: "2px 4px 2px" }}
+        value={textInput}
+        onChange={onTextInputChange}
+      ></Textfield>
+      <TodoList todoList={todoList} onCheckBtnClick={onCheckBtnClick} />
+    </>
+  );
+}
+
+export default App;
